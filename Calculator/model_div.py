c5d9fd3b4b176ea127bb329eeb6c4e57ddb19617
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-21 01:04:47 +0500

    Added changes.

--- Calculator/model_div.py
+++ Calculator/model_div.py
@@ -1,7 +1,12 @@
 def Division(divisible1, divider1, divisible2, divider2, key):
+    try:
+        divisible1/divider1
+    except ZeroDivisionError:
+        key = 4
     dictionary = {
         1: f"{divisible1/divider1}",
-        2: f"{complex(divisible1, divider1)/complex(divisible2, divider2)}".replace('(', '').replace(')', ''),
-        3: f"{divisible1%divider1}"
+        # 2: f"{complex(divisible1, divider1)/complex(divisible2, divider2)}".replace('(', '').replace(')', ''),
+        3: f"{divisible1%divider1}",
+        4: "0"
     }
     return dictionary[key]
